cb360f0d47600704d7e92dc8a8fc9be83b23da98
Gabriela Tavares <88911922+GabiTavaresV@users.noreply.github.com>
2024-09-14 17:12:31 -0300

    feat: add typeORM connection management

---
+++ src/database/connection-typeorm.ts
@@ -0,0 +1,30 @@
+import { createConnection, DataSource } from "typeorm";
+import { config } from "../config/typeorm-config";
+
+export class ConnectionTypeORM {
+  private static instance: ConnectionTypeORM;
+  private con!: DataSource;
+
+  private constructor() {}
+
+  public static getInstance(): ConnectionTypeORM {
+    if (!ConnectionTypeORM.instance) {
+      ConnectionTypeORM.instance = new ConnectionTypeORM();
+    }
+
+    return ConnectionTypeORM.instance;
+  }
+
+  public async startConnection(): Promise<void> {
+    console.log("Stating connection TypeORM");
+    this.con = await createConnection(config());
+    console.info("Finished connection TypeORM");
+  }
+
+  public async getConnection(): Promise<DataSource> {
+    if (!this.con) {
+      await this.startConnection();
+    }
+    return this.con;
+  }
+}
