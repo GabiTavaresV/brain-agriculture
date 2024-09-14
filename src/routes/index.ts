e24efcee9d90ce877a004c084cc6a1e8036c327c
Gabriela Tavares <88911922+GabiTavaresV@users.noreply.github.com>
2024-09-14 17:17:27 -0300

    feat: add agriculture, dashboard and login router

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { agricultureRouter } from "../rural-producer/routes/rural-producer";
+import { dashboardRouter } from "../rural-producer/routes/dashboard";
+import { loginRouter } from "../rural-producer/routes/login";
+
+export const routes = Router();
+
+routes.use("/agriculture", agricultureRouter);
+routes.use("/dashboard", dashboardRouter);
+routes.use("/login", loginRouter);
