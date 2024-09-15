8e3edcdfdf0c18a878e9ad80c40a6300933993bb
Gabriela Tavares <88911922+GabiTavaresV@users.noreply.github.com>
2024-09-15 11:41:47 -0300

    config: add lint and prettier

--- src/rural-producer/service/register-rural-producer-service.ts
+++ src/rural-producer/service/register-rural-producer-service.ts
@@ -1,21 +1,16 @@
-import { IFarmData, IRegisterRuralProducer } from "../interfaces/interfaces";
-import { BaseService } from "./base-service";
+import { BaseService } from './base-service';
+import { IFarmData, IRegisterRuralProducer } from '../interfaces/interfaces';
 
-export class RegisterRuralProducerService extends BaseService<
-  IRegisterRuralProducer,
-  IFarmData
-> {
+export class RegisterRuralProducerService extends BaseService<IRegisterRuralProducer, IFarmData> {
   public async execute(input: IRegisterRuralProducer): Promise<IFarmData> {
     const { taxId, arableArea, vegetationArea, totalFarmArea } = input;
 
     if (await this.checkIfExists(taxId)) {
-      throw new Error("O tax_id informado já está cadastrado.");
+      throw new Error('O tax_id informado já está cadastrado.');
     }
 
     if (arableArea + vegetationArea > totalFarmArea) {
-      throw new Error(
-        "A soma da área agricultável e da vegetação não pode ser maior que a área total da fazenda."
-      );
+      throw new Error('A soma da área agricultável e da vegetação não pode ser maior que a área total da fazenda.');
     }
 
     return this.repository.create(input);
