ac903d236f7ed27acf96436febfc12837c538745
Gabriela Tavares <88911922+GabiTavaresV@users.noreply.github.com>
2024-09-15 15:13:45 -0300

    test: add update rural producer service

---
+++ src/rural-producer/service/test/update-rural-producer-service.spec.ts
@@ -0,0 +1,63 @@
+import { IUpdateParams } from '../../interfaces/interfaces';
+import { UpdateRuralProducerService } from '../update-rural-producer-service';
+
+describe('UpdateRuralProducerService', () => {
+  let service: UpdateRuralProducerService;
+  let repositoryMock: any;
+
+  beforeEach(() => {
+    repositoryMock = {
+      findById: jest.fn(),
+      update: jest.fn(),
+    };
+    service = new UpdateRuralProducerService(repositoryMock);
+  });
+
+  it('should throw an error if no parameters are provided', async () => {
+    await expect(service.execute({} as IUpdateParams)).rejects.toThrow(
+      'Pelo menos um parâmetro deve ser fornecido para atualização.',
+    );
+  });
+
+  it('should throw an error if the record is not found', async () => {
+    repositoryMock.findById.mockResolvedValue(null);
+
+    await expect(service.execute({ id: 1, arableArea: 500 } as unknown as IUpdateParams)).rejects.toThrow(
+      'Registro não encontrado.',
+    );
+  });
+
+  it('should throw an error if the sum of arable and vegetation areas exceeds the total farm area', async () => {
+    repositoryMock.findById.mockResolvedValue({
+      total_farm_area: 100,
+      arable_area: 50,
+      vegetation_area: 20,
+    });
+
+    await expect(
+      service.execute({
+        id: '1',
+        arableArea: 500,
+        vegetationArea: 30,
+      } as IUpdateParams),
+    ).rejects.toThrow('A soma da área agricultável e da vegetação não pode ser maior que a área total da fazenda.');
+  });
+
+  it('should call the update method with the correct parameters when all parameters are valid', async () => {
+    repositoryMock.findById.mockResolvedValue({
+      total_farm_area: 200,
+      arable_area: 50,
+      vegetation_area: 50,
+    });
+
+    const updateInput: IUpdateParams = {
+      id: '1',
+      arableArea: 60,
+      vegetationArea: 60,
+    };
+
+    await service.execute(updateInput);
+
+    expect(repositoryMock.update).toHaveBeenCalledWith(updateInput);
+  });
+});
