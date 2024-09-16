22cf80ce66c2c964c5d3e2f98847936e95f03c8d
Gabriela Tavares <88911922+GabiTavaresV@users.noreply.github.com>
2024-09-16 10:15:15 -0300

    fiz: add planted crops validation

--- src/rural-producer/service/register-rural-producer-service.ts
+++ src/rural-producer/service/register-rural-producer-service.ts
@@ -1,12 +1,19 @@
 import { BaseService } from './base-service';
 import { formatTaxId } from '../../utils/format-taxId';
+import { validateCrops } from '../../utils/validate-crops';
 import { FarmResponse, RegisterRuralProducerParams } from '../interfaces/interfaces';
 
 export class RegisterRuralProducerService extends BaseService<RegisterRuralProducerParams, FarmResponse> {
   public async execute(input: RegisterRuralProducerParams): Promise<FarmResponse> {
-    const { taxId, arableArea, vegetationArea, totalFarmArea } = input;
+    const { taxId, arableArea, vegetationArea, totalFarmArea, plantedCrops } = input;
 
     const formattedTaxId = formatTaxId(taxId);
+
+    const lowerCaseCrops = plantedCrops.map((crop) => crop.toLowerCase());
+
+    if (!validateCrops(lowerCaseCrops)) {
+      throw new Error('Culturas plantadas não faz parte da listagem');
+    }
 
     if (await this.checkIfExists(formattedTaxId)) {
       throw new Error('O tax_id informado já está cadastrado.');
